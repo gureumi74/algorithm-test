df72b669d24a3a4c4d61aae69cabc86487e25c20
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-01 22:36:11 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 248 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for(int i = 1; i <= input; i++) {
+            System.out.printf("*".repeat(i));
+            System.out.printf(" ".repeat((input - i) * 2));
+            System.out.println("*".repeat(i));
+        }
+        for(int i = 1; i < input; i++) {
+            System.out.print("*".repeat(input - i));
+            System.out.print(" ".repeat(2 * i));
+            System.out.println("*".repeat(input - i));
+        }
+    }
+}
