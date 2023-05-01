06561a7f10c5600a74fdc23915f0d3bdb2ddfd4a
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-01 20:08:46 +0900

    [Bronze III] Title: 홀수, Time: 144 ms, Memory: 15796 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int i = 0; i < 7; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n % 2 != 0) {
+                sum += n;
+                min = min < n ? min : n;
+            }
+        }
+
+        if(sum == 0) {
+            bw.write("-1");
+        } else {
+            bw.write(sum + "\n");
+            bw.write(min + "\n");
+        }
+        bw.close();
+    }
+}
