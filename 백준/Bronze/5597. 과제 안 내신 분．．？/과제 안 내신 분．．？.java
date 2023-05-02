7902f8ca6f06f6a5a38ce8d0244e0221d6c88481
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-02 11:29:10 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] attendanceStudent = new int[30];
+        for(int i = 0; i < 28; i++) {
+            attendanceStudent[Integer.parseInt(br.readLine()) - 1] = 1;
+        }
+        for(int i = 1; i <= 30; i++) {
+            if (attendanceStudent[i - 1] == 0) {
+                bw.write(String.valueOf(i) + "\n");
+            }
+        }
+
+        bw.close();
+    }
+}
