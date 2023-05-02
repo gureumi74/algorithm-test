903fb21149378bdfa99c09f8a0daf257486e6cbb
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-02 11:51:07 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> list = new ArrayList();
+        for(int i = 0; i < 10; i++) {
+            int n = Integer.parseInt(br.readLine()) % 42;
+            if(!list.contains(n)) {
+                list.add(n);
+            }
+        }
+        bw.write(String.valueOf(list.size()));
+        bw.close();
+    }
+}
