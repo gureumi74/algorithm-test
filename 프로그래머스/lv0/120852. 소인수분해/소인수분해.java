3792c2bb1b3d7b3eda7d0b056ee43f315d998bb3
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-04 09:31:04 +0900

    [level 0] Title: 소인수분해, Time: 0.09 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -1,16 +1,41 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int n) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                set.add(i);
-                n /= i;
+    public int[] solution(int n) {
+        int[] answer = new int[n];
+        int count = 0;
+        // 소인수 분해는 소수들로 나누어 떨어질 때까지 구하는 것이기 때문에 i * i < n 까지 돌면 15의 경우
+        // 기존 아래 for문은 2~3까지밖에 동작하지 않음 i < n으로 수정
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                answer[count++] = i;
             }
         }
-        List<Integer> list = new ArrayList<>(set);
-        list.sort(Comparator.naturalOrder());
-        return list;
+        // count = 0 일 경우 자기 자신을 리턴하면 되므로 return n;
+        if (count == 0) {
+            answer = new int[1];
+            answer[0] = n;
+            return answer;
+        }
+
+        int isPrime = 0;
+
+        int[] result = new int [count];
+
+        for (int i = 0; i < count; i++) {
+            if(getInts(answer[i])){
+                result[isPrime++] = answer[i];
+            }
+        }
+
+        return Arrays.copyOf(result, isPrime);
+    }
+    private static boolean getInts(int num) {
+        for (int j = 2; j < num ; j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
