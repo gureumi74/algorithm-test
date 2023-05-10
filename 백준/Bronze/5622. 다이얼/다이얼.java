b115af26710045f7b270c95bf6859ff3be717926
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-10 17:12:19 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+        int sum = 0;
+
+
+        for(int i = 0; i < input.length; i++) {
+            switch (input[i]) {
+                case 'A' : 
+                case 'B' :
+                case 'C' : 
+                    sum += 3;
+                    break;
+                case 'D' :
+                case 'E' : 
+                case 'F' :
+                    sum += 4;
+                    break;
+                case 'G' : 
+                case 'H' : 
+                case 'I' : 
+                    sum += 5;
+                    break;
+                case 'J' : 
+                case 'K' : 
+                case 'L' : 
+                    sum += 6;
+                    break;
+                case 'M' : 
+                case 'N' : 
+                case 'O' : 
+                    sum += 7;
+                    break;
+                case 'P' : 
+                case 'Q' : 
+                case 'R' : 
+                case 'S' :
+                    sum += 8;
+                    break;
+                case 'T' : 
+                case 'U' : 
+                case 'V' : 
+                    sum += 9;
+                    break;
+                default :
+                    sum += 10;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
