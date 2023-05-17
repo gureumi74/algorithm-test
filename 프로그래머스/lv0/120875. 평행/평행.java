a08b6be2f5a5c6013836862511f8cf9464231e5d
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-17 17:45:46 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[][] dots) {
-         int[][] location = new int[3][4];
+         // 기울기 공식 (y2 - y1) / (x2 - x1)
+        int[][] location = new int[3][4];
 
+        // 두 점씩 잇는 3가지 경우의 수를 담은 배열
         location[0] = new int[]{dots[0][0] - dots[1][0], dots[0][1] - dots[1][1], dots[2][0] - dots[3][0], dots[2][1] - dots[3][1]};
         location[1] = new int[]{dots[0][0] - dots[2][0], dots[0][1] - dots[2][1], dots[1][0] - dots[3][0], dots[1][1] - dots[3][1]};
         location[2] = new int[]{dots[0][0] - dots[3][0], dots[0][1] - dots[3][1], dots[2][0] - dots[1][0], dots[2][1] - dots[1][1]};
 
+        // 배열을 돌면서 기울기 공식이 성립하는지 확인, 맞으면 1, for문이 돌아도 아니면 0 리턴
         for(int i = 0; i < location.length; i++) {
             if((float) location[i][1] / location[i][0] == (float) location[i][3] / location[i][2]) return 1;
         }
