f84267280f93f1592f8cf198e7be84230952de3a
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-05-23 18:32:25 +0900

    [level 0] Title: 등수 매기기, Time: 0.40 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -2,17 +2,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
-        float[] average = new float[score.length];
-        List<Float> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for(int i = 0; i < score.length; i++) {
-            average[i] = (float) (score[i][0] + score[i][1]) / 2;
-            list.add(average[i]);
+            list.add(score[i][0] + score[i][1]);
         }
 
         Collections.sort(list, Collections.reverseOrder());
 
         for(int i = 0; i < score.length; i++) {
-            answer[i] = list.indexOf(average[i]) + 1;
+            answer[i] = list.indexOf(score[i][0] + score[i][1]) + 1;
         }
 
         return answer;
