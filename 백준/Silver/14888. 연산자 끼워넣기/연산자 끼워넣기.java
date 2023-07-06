5952f6af49ae8c962479c9ae451be97f454b84b0
하늘 <112999436+gureumi74@users.noreply.github.com>
2023-07-06 11:13:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 144 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,32 +5,24 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] operator;
+    static int[] operator = new int[4];
     static int nums[], n, operatorArr[];
-    static boolean visit[];
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
-    static boolean check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
         nums = new int[n];;
-        operator = new int[n - 1];
         operatorArr = new int[n - 1];
-        visit = new boolean[n - 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        int count = 0;
         for(int i = 0; i < 4; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            for(int j = 0; j < tmp; j++) {
-                operator[count++] = i;
-            }
+            operator[i] = Integer.parseInt(st.nextToken());
         }
 
         backTracking(0);
@@ -40,20 +31,20 @@ public class Main {
     }
     static void backTracking(int k) {
         if(k == n - 1) {
+//            for(int i : operatorArr) {
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
             calculate();
-//                for (int i : operatorArr) {
-//                    System.out.print(i + " ");
-//                }
-//                System.out.println();
             return;
         }
 
-        for(int i = 0; i < n - 1; i++) {
-            if(!visit[i]) {
-                operatorArr[k] = operator[i];
-                visit[i] = true;
+        for(int i = 0; i < 4; i++) {
+            if(operator[i] > 0) {
+                operatorArr[k] = i;
+                operator[i]--;
                 backTracking(k + 1);
-                visit[i] = false;
+                operator[i]++;
             }
         }
     }
