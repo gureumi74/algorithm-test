a8eee7f3101af883509dd3e0ce238e9b1a85aba9
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-09-04 16:45:48 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,21 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
+        String str = "*";
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println(str.repeat(i + 1));
         }
     }
 }
