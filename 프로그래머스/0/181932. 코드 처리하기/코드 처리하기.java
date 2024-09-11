14b67ae2ebe57aa1ca90d09baa72931536de1e59
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-09-11 17:53:39 +0900

    [level 0] Title: 코드 처리하기, Time: 363.81 ms, Memory: 371 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        boolean mode = false;
+        for(int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                mode = mode ? false : true;
+            } else if (mode && i % 2 != 0) {
+                answer += code.charAt(i);
+            } else if (!mode && i % 2 == 0) {
+                answer += code.charAt(i);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer;
+    }
+}
