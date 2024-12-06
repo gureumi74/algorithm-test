9b1f343df257efe64f7b770b9570daaad41fc1c7
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-12-06 17:57:51 +0900

    [Silver II] Title: 스택 수열, Time: 1280 ms, Memory: 43524 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,40 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        ArrayList<String> result = new ArrayList<>();
-        int caseNum = Integer.parseInt(br.readLine());
 
-        int idx = 0;
-        boolean noCheck = false;
-        for(int i = 0; i < caseNum; i++) {
-            int input = Integer.parseInt(br.readLine());
-            if(input > idx) {
-                for(int j = idx + 1; j < input + 1; j++) {
+        int element = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num > element) {
+                for(int j = element + 1; j <= num; j++) {
                     stack.push(j);
-                    result.add("+");
+                    list.add("+");
                 }
-                idx = input;
-            } else if (stack.peek() != input) {
-                noCheck = true;
-                break;
+                element = num;
+            } else {
+                if (stack.peek() != num) {
+                    list.clear();
+                    break;
+                }
             }
 
             stack.pop();
-            result.add("-");
+            list.add("-");
         }
 
-        if (noCheck) {
+        if(list.isEmpty()) {
             System.out.println("NO");
         } else {
-            for (String s : result) {
-                System.out.println(s);
+            for (String result : list) {
+                System.out.println(result);
             }
         }
     }
