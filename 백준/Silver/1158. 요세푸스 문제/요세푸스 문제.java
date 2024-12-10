272e7936345ddb5ca4d236a820a9750fd6af16a2
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-12-10 23:03:59 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 172 ms, Memory: 16756 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,43 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
-class Node {
-    int value;
-    Node next;
-
-    public Node(int value) {
-        this.value = value;
-        this.next = null;
-    }
-}
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        int inputNum = Integer.parseInt(input[0]);
-        int deleteNum = Integer.parseInt(input[1]);
-
-        Node head = new Node(1);
-        Node cur = head;
-        for(int i = 2; i <= inputNum; i++) {
-            cur.next = new Node(i);
-            cur = cur.next;
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i = 1; i <= n; i++) {
+            list.add(i);
         }
-        cur.next = head;
 
         StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        // 리스트에 한 명만 남을 때까지 반복
-        while (cur != cur.next) {
-            for(int i = 0; i < deleteNum - 1; i++) {
-                cur = cur.next;
-            }
-            sb.append(cur.next.value).append(", ");
-            cur.next = cur.next.next;
+        int idx = k - 1;
+        int num = list.get(idx); // 초기값
+        sb.append("<" + num);
+        list.remove(idx);
+
+        while (list.size() > 0) {
+            idx = (idx + k - 1) % list.size();
+            num = list.get(idx);
+            sb.append(", " + num);
+            list.remove(idx);
         }
 
-        sb.append(cur.value).append(">");
-        System.out.println(sb.toString());
+        sb.append(">");
+        System.out.println(sb);
     }
 }
