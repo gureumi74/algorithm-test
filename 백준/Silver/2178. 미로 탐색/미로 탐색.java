e061f87ee694709df675d2e46ae2981e8e342f33
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-12-13 01:04:34 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 16572 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,46 +1,53 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    public void solution(int[][] board) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        boolean[][] visit = new boolean[board.length][board[0].length];
-        int[][] moveCount = new int[board.length][board[0].length];
-        Queue<ArrayList<Integer>> queue = new LinkedList<>();
-        queue.add(new ArrayList<>(Arrays.asList(0, 0)));
-        visit[0][0] = true;
-        moveCount[0][0] = 1;
-        while (!queue.isEmpty()) {
-            ArrayList<Integer> cur = queue.poll();
-            for(int i = 0; i < 4; i++) {
-                int nx = cur.get(0) + dx[i];
-                int ny = cur.get(1) + dy[i];
-                if(nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length) continue;
-                if(visit[nx][ny] || board[nx][ny] != 1) continue;
-                visit[nx][ny] = true;
-                moveCount[nx][ny] = moveCount[cur.get(0)][cur.get(1)] + 1;
-                queue.add(new ArrayList<Integer>(Arrays.asList(nx, ny)));
-            }
+    static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        bw.write(String.valueOf(moveCount[board.length - 1][board[0].length - 1]));
-        bw.close();
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][M];
-        for(int i = 0; i < N; i++) {
-            String[] input = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(input[j]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        int[][] board = new int[n][m];
+        int[][] maze = new int[n][m]; // BFS 이용해서 최단 경로 담을 배열
+        for (int i = 0; i < n; i++) {
+            input = br.readLine().split("");
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(input[j]);
             }
         }
 
-        Main baekjoon2178 = new Main();
-        baekjoon2178.solution(arr);
+        Queue<Pair> queue = new LinkedList<>();
+        int[][] location = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        queue.add(new Pair(0, 0));
+        board[0][0] = 0;
+        // 자기 칸 포함 계산
+        maze[0][0] = 1;
+
+        while (!queue.isEmpty()) {
+            Pair cur = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + location[i][0];
+                int ny = cur.y + location[i][1];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if(board[nx][ny] != 1) continue;
+                board[nx][ny] = 0;
+                maze[nx][ny] = maze[cur.x][cur.y] + 1;
+                queue.add(new Pair(nx, ny));
+            }
+        }
+
+        bw.write(String.valueOf(maze[n - 1][m - 1]));
+        bw.close();
     }
 }
