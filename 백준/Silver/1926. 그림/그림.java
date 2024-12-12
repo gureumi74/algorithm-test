2dbedd621e14e5edc4b584ddde2cd8516b783dc8
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-12-13 00:49:38 +0900

    [Silver I] Title: 그림, Time: 592 ms, Memory: 48760 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,57 +1,66 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public void BFSSolution(int[][] board) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        boolean[][] visit = new boolean[board.length][board[0].length];
-        int max = 0; // 그림의 최댓값
-        int num = 0; // 그림의 개수
+    static class Pair {
+        int x;
+        int y;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 0 || visit[i][j]) continue;
-                num++;
-                Queue<ArrayList<Integer>> queue = new LinkedList<>();
-                visit[i][j] = true;
-                queue.add(new ArrayList<>(Arrays.asList(i, j)));
-
-                int area = 0;
-                while (!queue.isEmpty()) {
-                    area++;
-                    ArrayList<Integer> cur = queue.poll();
-                    for (int k = 0; k < 4; k++) {
-                        int nx = cur.get(0) + dx[k];
-                        int ny = cur.get(1) + dy[k];
-                        if (nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length) continue;
-                        if (visit[nx][ny] || board[nx][ny] != 1) continue;
-                        visit[nx][ny] = true;
-                        queue.add(new ArrayList<>(Arrays.asList(nx, ny)));
-                    }
-                }
-                if (max < area) max = area;
-            }
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        bw.write(num + "\n" + max);
-        bw.close();
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int h = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[h][w];
-        for(int i = 0; i < h; i++) {
+        int n = Integer.parseInt(st.nextToken()); // 세로 크기
+        int m = Integer.parseInt(st.nextToken()); // 가로 크기
+        int[][] board = new int[n][m]; // 그림 보드
+
+        // 그림 업데이트
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < w; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        Main baekjoon1926 = new Main();
-        baekjoon1926.BFSSolution(arr);
+        int cnt = 0; // 그림의 개수
+        int max = 0; // 가장 큰 그림의 넓이
+        int[][] location = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 우 하 좌 상
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(board[i][j] == 1) {
+                    cnt++;
+                    Queue<Pair> queue = new LinkedList<>();
+                    int extent = 1; // 그림 넓이
+                    queue.add(new Pair(i, j));
+                    board[i][j] = 0;
+
+                    while (!queue.isEmpty()) {
+                        Pair cur = queue.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = cur.x + location[k][0];
+                            int ny = cur.y + location[k][1];
+                            if (nx < 0 || ny < 0 || nx >= n | ny >= m) continue;
+                            if (board[nx][ny] != 1) continue;
+                            board[nx][ny] = 0;
+                            queue.add(new Pair(nx, ny));
+                            extent++;
+                        }
+                    }
+
+                    max = Math.max(max, extent);
+                }
+            }
+        }
+        
+        bw.write(cnt + "\n" + max);
+        bw.close();
     }
 }
