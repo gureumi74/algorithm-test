8b506f9207cfe1bf17087190c25fdb3e7030adb2
하늘 <112999436+gureumi74@users.noreply.github.com>
2024-12-26 02:01:45 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 18924 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, arr[], input[];
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        input = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            input[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(input);
-        solution(0, 0);
-        System.out.println(sb);
-    }
-    static void solution(int k, int start) {
-        if(k == M) {
-            for(int val : arr) {
-                sb.append(val + " ");
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m, select[], arr[];
+    public static void solution(int depth, int idx) throws IOException {
+        if (depth == m) {
+            for (int num: select) {
+                bw.write(num + " ");
             }
-            sb.append("\n");
+            bw.write("\n");
             return;
         }
 
-        for(int i = start; i < N; i++) {
-            arr[k] = input[i];
-            solution(k + 1, start);
-            // 1로 시작하는 수열을 시작한 뒤 1이 나오는 경우는 없으니까 start 증가
-            start++;
+        for(int i = idx; i < n; i++) {
+            select[depth] = arr[i];
+            solution(depth + 1, i);
         }
+    }
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        select = new int[m];
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        solution(0, 0);
+        bw.close();
     }
 }
