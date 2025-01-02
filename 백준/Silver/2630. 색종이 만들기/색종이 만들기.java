174aa4a3c39694cf0aae63f9a17a9a04d92ddd9c
하늘 <112999436+gureumi74@users.noreply.github.com>
2025-01-03 01:00:42 +0900

    [Silver II] Title: 색종이 만들기, Time: 168 ms, Memory: 16632 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,46 +1,50 @@
+import java.awt.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    // 아멘.....
-    static int N, map[][], white = 0, blue = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+    static int blue, white, paper[][];
+    public static boolean check(int x, int y, int size) {
+        int n = paper[x][y];
+        for(int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (paper[i][j] != n) {
+                    return false;
+                }
             }
         }
-        solution(0, 0, N);
-        bw.write(white + "\n");
-        bw.write(blue + "\n");
-        bw.close();
+        return true;
     }
-    static void solution(int r, int c, int size) {
-        if(check(r, c, size)) {
-            if(map[r][c] == 0) white++;
-            if(map[r][c] == 1) blue++;
-            return;
+    public static void solution(int x, int y, int size) {
+        if(check(x, y, size)) {
+           if (paper[x][y] == 0) white++;
+           if (paper[x][y] == 1) blue++;
+           return;
         }
 
         int newSize = size / 2;
-        for(int i = r; i < r + size; i += newSize) {
-            for(int j = c; j < c + size; j += newSize) {
+
+        for (int i = x; i < x + size; i += newSize) {
+            for (int j = y; j < y + size; j += newSize) {
                 solution(i, j, newSize);
             }
         }
+
     }
-    static boolean check(int r, int c, int size) {
-        for(int i = r; i < r + size; i++) {
-            for(int j = c; j < c + size; j++) {
-                if(map[r][c] != map[i][j]) return false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        paper = new int[n][n];
+        String[] input;
+        for(int i = 0; i < n; i++) {
+            input = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                paper[i][j] = Integer.parseInt(input[j]);
             }
         }
-
-        return true;
+        solution(0, 0, n);
+        bw.write(white + "\n");
+        bw.write(blue + "\n");
+        bw.close();
     }
 }
